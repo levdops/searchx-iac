603a8a56d7fa8e483bdc78fe6566745d53fa2eef
Vigo Vlugt <vigovlugt@gmail.com>
2024-02-15 15:48:12 +0100

    Add cloudfront to iac

--- index.ts
+++ index.ts
@@ -2,41 +2,94 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-const bucket = new aws.s3.Bucket("searchx-frontend", {
+const frontendBucket = new aws.s3.Bucket("searchx-frontend", {
     website: {
         indexDocument: "index.html",
     },
 });
 
-const ownershipControls = new aws.s3.BucketOwnershipControls(
-    "ownership-controls",
+const originAccessIdentity = new aws.cloudfront.OriginAccessIdentity(
+    "originAccessIdentity",
     {
-        bucket: bucket.id,
-        rule: {
-            objectOwnership: "ObjectWriter",
+        comment: "this is needed to setup s3 polices and make s3 not public.",
+    }
+);
+
+const cdn = new aws.cloudfront.Distribution("cdn", {
+    enabled: true,
+    // Alternate aliases the CloudFront distribution can be reached at, in addition to https://xxxx.cloudfront.net.
+    // Required if you want to access the distribution via config.targetDomain as well.
+    // aliases: distributionAliases,
+
+    // We only specify one origin for this distribution, the S3 content bucket.
+    origins: [
+        {
+            originId: frontendBucket.arn,
+            domainName: frontendBucket.bucketRegionalDomainName,
+            s3OriginConfig: {
+                originAccessIdentity:
+                    originAccessIdentity.cloudfrontAccessIdentityPath,
+            },
         },
-    }
-);
+    ],
 
-const publicAccessBlock = new aws.s3.BucketPublicAccessBlock(
-    "public-access-block",
-    {
-        bucket: bucket.id,
-        blockPublicAcls: false,
-    }
-);
+    defaultRootObject: "index.html",
 
-const bucketObject = new aws.s3.BucketObject(
-    "index.html",
-    {
-        bucket: bucket.id,
-        source: new pulumi.asset.FileAsset("./index.html"),
-        contentType: "text/html",
-        acl: "public-read",
+    // A CloudFront distribution can configure different cache behaviors based on the request path.
+    // Here we just specify a single, default cache behavior which is just read-only requests to S3.
+    defaultCacheBehavior: {
+        targetOriginId: frontendBucket.arn,
+
+        viewerProtocolPolicy: "redirect-to-https",
+        allowedMethods: ["GET", "HEAD", "OPTIONS"],
+        cachedMethods: ["GET", "HEAD", "OPTIONS"],
+
+        forwardedValues: {
+            cookies: { forward: "none" },
+            queryString: false,
+        },
+
+        minTtl: 0,
+        defaultTtl: 60 * 10,
+        maxTtl: 60 * 10,
     },
-    { dependsOn: [publicAccessBlock, ownershipControls] }
-);
 
-export const bucketName = bucket.id;
+    // "All" is the most broad distribution, and also the most expensive.
+    // "100" is the least broad, and also the least expensive.
+    priceClass: "PriceClass_100",
 
-export const bucketEndpoint = pulumi.interpolate`http://${bucket.websiteEndpoint}`;
+    // You can customize error responses. When CloudFront receives an error from the origin (e.g. S3 or some other
+    // web service) it can return a different error code, and return the response for a different resource.
+    // customErrorResponses: [
+    //     { errorCode: 404, responseCode: 404, responsePagePath: "/404.html" },
+    // ],
+
+    restrictions: {
+        geoRestriction: {
+            restrictionType: "none",
+        },
+    },
+
+    viewerCertificate: {
+        // acmCertificateArn: certificateArn,  // Per AWS, ACM certificate must be in the us-east-1 region.
+        cloudfrontDefaultCertificate: true,
+        sslSupportMethod: "sni-only",
+    },
+
+    // loggingConfig: {
+    //     bucket: logsBucket.bucketDomainName,
+    //     includeCookies: false,
+    //     prefix: `${config.targetDomain}/`,
+    // },
+});
+
+const bucketObject = new aws.s3.BucketObject("index.html", {
+    bucket: frontendBucket.id,
+    source: new pulumi.asset.FileAsset("./index.html"),
+    contentType: "text/html",
+    acl: "public-read",
+});
+
+export const bucketName = frontendBucket.id;
+
+export const cloudFrontDomain = cdn.domainName;
